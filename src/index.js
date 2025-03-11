92cc4006614aaa5f232c01e319102f91d91a6774
Richard Nixon <rnixonaf@gmail.com>
2025-03-11 23:41:19 +0000

    until interation 3

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
-
+let showToastDiv = null;
+const startButton = document.getElementById('start-btn');
+const timerElement = document.getElementById('time');
 
 
 // ITERATION 1: Add event listener to the start button
+startButton.addEventListener('click', () => {
+  startCountdown();
+});
 
-// Your code goes here ...
 
 
+function timerCount() {
+  if (remainingTime > 0) {
+    remainingTime--; 
+    timerElement.innerText = remainingTime;
+  } else {
+    clearInterval(timer); 
+  }
+  if (remainingTime == 3){
+    showToast();
+  }
+
+}
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
+  startButton.disabled = true;
+  remainingTime = DURATION;
+  timerElement.innerText = remainingTime;
 
+  clearInterval(timer);
 
-  // Your code goes here ...
+  timer = setInterval(timerCount, 1000);
 }
 
 
@@ -26,13 +46,10 @@ function startCountdown() {
 function showToast(message) {
   console.log("showToast called!");
 
-  // Your code goes here ...
-
-
+  document.getElementById('toast').className = "show", 3000;
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
-}
+};
